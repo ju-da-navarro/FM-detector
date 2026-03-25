c194e905b23640c6d63eb89160e806abf9e14df7
Arturo <ke.ar.panesso@unillanos.edu.co>
2026-03-25 13:58:09 -0500

    Upgrade of espectro.py, changes in interfaz.py for better visualization

--- espectro.py
+++ espectro.py
@@ -3,59 +3,54 @@ from statsmodels.tsa.stattools import acf
 
 LONGITUD = 44100*2 # 2 segundo
 
-
 def recortar(audio):
     if len(audio) >= LONGITUD:
         return audio[:LONGITUD]
     else:
         return np.pad(audio, (0, LONGITUD - len(audio)))
 
+#Función que calcula la diferencia con la media, la autocovarianza y la fft, retorna la fft
+def calcularFFT(audio):
 
-def espectro_promedio(audios, tipo):
-    espectros = []
-
-    for audio in audios:
-
-        # asegurar 1D (mono)
-        if len(audio.shape) > 1:
-            audio = np.mean(audio, axis=1)
-
-        # recorte
-        audio = recortar(audio)
-
-        x = audio - np.mean(audio)
-
-        # autocovarianza con acf (rápido con FFT)
-        autocov = acf(x, nlags=LONGITUD-1, fft=True)
-
-        # FFT + magnitud
-        fft = np.fft.rfft(autocov)  
-        espectros.append(np.abs(fft))
-
-    espectro = np.mean(espectros, axis=0)
-
-    #normalizar, opcional pero mejor
-    espectro = espectro / np.max(espectro)
-    
-    # persistencia
-    np.save(tipo, espectro)
-
-    return espectro
-
-
-def espectro_individual(audio):
+    #Asegurar audio mono para refinación de calculos
     if len(audio.shape) > 1:
         audio = np.mean(audio, axis=1)
 
+    #Recortar el audio
     audio = recortar(audio)
 
+    #Calcular la diferencia con la media
     x = audio - np.mean(audio)
 
+    #Calcular autocovarianza
     autocov = acf(x, nlags=LONGITUD-1, fft=True)
 
-    espectro = np.abs(np.fft.rfft(autocov))
+    #Retornar la fft de la autocovarianza previamente calculada
+    return np.fft.rfft(autocov)
 
-    if np.max(espectro) != 0:
-        espectro = espectro / np.max(espectro)
+#Función que procesa en caso de que se reciba una lista para entrenamiento o un único audio para clasificación en tiempo real
+def determinar_espectro(audio_data, tipo = ""):
 
-    return espectro
+    #En caso de que procesemos una lista de audios (Generalmente es para entrenar)
+    if isinstance(audio_data, list):
+        espectros = []
+
+        for audio in audio_data:
+            espectros.append(np.abs(calcularFFT(audio)))
+
+        #Calcular promedio
+        espectro = np.mean(espectros, axis = 0)
+
+        #Normalización
+        espectro /= np.max(espectro)
+
+        #Almacenar
+        if tipo:
+           np.save(tipo, espectro)
+    
+        return espectro
+    
+    #En caso de que se ingrese un valor único (Generalmente es para clasificar)
+    espectro = np.abs(calcularFFT(audio_data))
+
+    return (espectro / np.max(espectro)) if (np.max(espectro) != 0) else espectro
